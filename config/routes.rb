959da14164963f7c900638d90967a13961a34b5f
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-14 18:14:26 +0000

    Add password_resets_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/login' => "user_sessions#new", as: :login
   resources :users
   resources :user_sessions, only: [:new, :create]
-  
+  resources :password_resets, only: [:new]
   
   resources :todo_lists do 
     resources :todo_items do 
